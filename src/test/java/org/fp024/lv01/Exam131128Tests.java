a9437af149b3f25358f9331be4f3c5d06523caf3
fp024 <aopenasus@naver.com>
2025-05-26 21:18:52 +0900

    숫자 짝꿍(진행중) - 131128
    TreeMap으로 바꾸고 좀 속도는 나아졌을 것이라 생각을 했는데...
    
    그런데 이번에는...
    - 6~15은 런타임 에러 😂😂😂
    
    아마도 마지막에 Integer 캐스팅해서 0을 제거해볼려고 한 동작에 문제가 있어보인다.

--- src/test/java/org/fp024/lv01/Exam131128Tests.java
+++ src/test/java/org/fp024/lv01/Exam131128Tests.java
@@ -2,10 +2,7 @@ package org.fp024.lv01;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.TreeMap;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,40 +17,43 @@ class Exam131128Tests {
   static class Solution {
     public String solution(String x, String y) {
       String answer;
-
-      List<String> descX =
-          Arrays.stream(x.split("")).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-
-      List<String> descY =
-          Arrays.stream(y.split("")).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-
-      final List<String> longer;
-      final List<String> shorter;
-
-      if (descX.size() > descY.size()) {
-        longer = descX;
-        shorter = descY;
-      } else {
-        longer = descY;
-        shorter = descX;
-      }
-
       StringBuilder result = new StringBuilder();
 
-      for (String s : shorter) {
-        int foundIndex = longer.indexOf(s);
-        if (foundIndex != -1) {
-          longer.remove(foundIndex);
-          result.append(s);
+      // 1. 문자, 카운트의 트리맵 생성, 순서는 문자가 큰 순서를 적용
+      TreeMap<Character, Integer> xMap = new TreeMap<>((o1, o2) -> o2 - o1);
+      TreeMap<Character, Integer> yMap = new TreeMap<>((o1, o2) -> o2 - o1);
+
+      for (int i = 0; i < x.length(); i++) {
+        xMap.put(x.charAt(i), xMap.getOrDefault(x.charAt(i), 0) + 1);
+      }
+
+      for (int i = 0; i < y.length(); i++) {
+        yMap.put(y.charAt(i), yMap.getOrDefault(y.charAt(i), 0) + 1);
+      }
+
+      // 2. 작은 맵을 찾아도 되긴 되는데... 키가 0~9까지 밖에 없어서 아무거나 먼저 해도 될 것 같다.
+
+      for (var xe : xMap.entrySet()) {
+        Character xKey = xe.getKey();
+        Integer xValue = xe.getValue();
+        int sameCount = 0;
+
+        if (yMap.containsKey(xKey)) {
+          Integer yValue = yMap.get(xKey);
+          sameCount = Math.min(xValue, yValue);
+        }
+
+        // 💡repeat를 쓰면 편한데...😅 Java 21부터 있다. 😂
+        // result.repeat(xKey, sameCount);
+        for (int i = 0; i < sameCount; i++) {
+          result.append(xKey);
         }
       }
 
-      if (result.length()
-          == 0) { // 💡Java 15부터는 StringBuilder에 isEmpty()가 있는데, 프로그래머스의 JDK는 Java 14라서 length로
-        // 검사해야한다.
-        answer = "-1";
+      if (result.length() > 0) {
+        answer = result.toString();
       } else {
-        answer = Integer.valueOf(result.toString()).toString();
+        answer = "-1";
       }
 
       return answer;
@@ -77,6 +77,18 @@ class Exam131128Tests {
   //   `.collect(Collectors.toList());`는 변경 가능 리스트
   //   `toList();`는 불변 리스트를 반환의 차이가 있었구나...
   //    불변으로 하면 remove 및 내용 변경(set)도 안된다.
+  //
+  //    아예 다른 방법을 생각한다면..
+  //    1. TreeMap에다 0 ~ 9의 갯수를 넣는다.
+  //       key 숫자 value 번호
+  //       결국 x, y에 대한 숫자갯수를 세어둔 TreeMap 2개가 만듬.
+  //
+  //    2. 동시에 존재해야하기 때문에 둘중 어느것을 먼저 세도 상관은 없어보이고,
+  //       큰 숫자부터 두 treeMap 몇개를 들고 있나 검사하면서 숫자를 만들어내면 될 것 같다. 😅
+  //
+  //     TreeMap으로 바꾸고도 또 틀렸는데...
+  //     x,y가 둘다 일치한다면 300만 자리의 수가 되므로 이걸 Integer로 바꿔보려해서 문제가 생긴게 맞는 것 같다.
+  //     그대로 출력하고 모든 수가 0으로 이루어졌을 때..만 0으로 잘 반환하게 처리하면 될 것 같다.
   //
   // === 다른 사람 풀이 확인 이후 의견 ===
   // ...
