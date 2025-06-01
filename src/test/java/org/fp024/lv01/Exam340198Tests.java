546598a92a19977732d5ab43a4597b27cdecb3c5
fp024 <aopenasus@naver.com>
2025-06-02 02:09:53 +0900

    [PCCE 기출문제] 10번 / 공원 - 340198 (통과)
    반례를 구해서 문제의 원인을 찾았다.
    현재위치가 정사각형 영역에 포함될 수 있는 1인지 검사를
    안하고 DP배열을 만들고 있었음..😂
    
    그부분을 해결하니 통과하였다.
    
    - +2점 통과

--- src/test/java/org/fp024/lv01/Exam340198Tests.java
+++ src/test/java/org/fp024/lv01/Exam340198Tests.java
@@ -34,19 +34,23 @@ class Exam340198Tests {
       //     현재 위치에서 좌/상/좌상과 각각 더했을 때 최소값이 현재 위치에서 가능한 정사각형 길이
       for (int i = 1; i < dp.length; i++) {
         for (int j = 1; j < dp[i].length; j++) {
-          int up = dp[i - 1][j];
-          int left = dp[i][j - 1];
-          int upLeft = dp[i - 1][j - 1];
-
-          dp[i][j] += Math.min(Math.min(up, left), upLeft);
+          // 💡현재 위치가 정사각형에 포함될 수 있는지 확인이 되야하므로, 1인지 검사해야한다.
+          if (dp[i][j] == 1) {
+            int up = dp[i - 1][j];
+            int left = dp[i][j - 1];
+            int upLeft = dp[i - 1][j - 1];
+            dp[i][j] += Math.min(Math.min(up, left), upLeft);
+          }
         }
       }
 
       // (3) 최대 정사각형 한변 얻기
       for (int i = 0; i < dp.length; i++) {
         for (int j = 0; j < dp[i].length; j++) {
+          System.out.print(dp[i][j] + " ");
           maxSquare = Math.max(maxSquare, dp[i][j]);
         }
+        System.out.println();
       }
 
       Arrays.sort(mats);
@@ -64,7 +68,7 @@ class Exam340198Tests {
   // ========== Test ==========
   @ParameterizedTest
   @MethodSource({
-    "org.fp024.lv01.Exam" + EXAM_NO + "TestData#defaultDataProvider", //
+    // "org.fp024.lv01.Exam" + EXAM_NO + "TestData#defaultDataProvider", //
     "org.fp024.lv01.Exam" + EXAM_NO + "TestData#extraDataProvider"
   })
   void testSolution(int[] mats, String[][] park, int expect) {
@@ -80,7 +84,9 @@ class Exam340198Tests {
   //   지금 문제를 위 문제와 비슷하게 풀면 쉽게 해결 될 것 같다.
   //   🤔 기본 테스트는 통과하는데, 제출시 다량 실패한다. 왜그럴까? 😂😂😂
   //
+  //   dp 배열을 구성할 때.. 현재 위치가 1임을 확인하지 않아서, 오류가 있었다. 😂
+  //
   // === 다른 사람 풀이 확인 이후 의견 ===
-  // ...
+  //   바로 이해는 안되지만.. 모두들 자신만의 스타일이 있는 것 같다. 👍👍
   //
 }
