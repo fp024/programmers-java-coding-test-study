419e3675afbec20e16d2491a357269b39b894691
fp024 <aopenasus@naver.com>
2025-06-02 01:42:50 +0900

    [PCCE 기출문제] 10번 / 공원 - 340198 (진행중)
    - 3, 5, 8, 10-14, 16, 17, 19, 20 테스트 케이스 실패중..
    
    그런데... 왜 실패하는지 아직 잘 모르겠다.

---
+++ src/test/java/org/fp024/lv01/Exam340198TestData.java
@@ -0,0 +1,93 @@
+package org.fp024.lv01;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+
+class Exam340198TestData {
+  // cspell:disable
+  static Stream<Arguments> defaultDataProvider() {
+    return Stream.of(
+        Arguments.of(
+            new int[] {5, 3, 2}, //
+            new String[][] {
+              {"A", "A", "-1", "B", "B", "B", "B", "-1"},
+              {"A", "A", "-1", "B", "B", "B", "B", "-1"},
+              {"-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"},
+              {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"},
+              {"D", "D", "-1", "-1", "-1", "-1", "-1", "F"},
+              {"D", "D", "-1", "-1", "-1", "-1", "E", "-1"}
+            },
+            3
+            //
+            )
+        //
+        );
+  }
+
+  static Stream<Arguments> extraDataProvider() {
+    return Stream.of(
+        Arguments.of(
+            new int[] {1, 2}, //
+            new String[][] {
+              {
+                "A", "-1",
+              },
+              {
+                "A", "-1",
+              }
+            },
+            1
+            //
+            ),
+        Arguments.of(
+            new int[] {3}, //
+            new String[][] {
+              {"-1", "-1", "-1"}, //
+              {"A", "-1", "-1"},
+              {"-1", "-1", "-1"}
+            },
+            -1
+            //
+            ),
+        Arguments.of(
+            new int[] {3}, //
+            new String[][] {
+              {"-1", "-1", "-1"}, //
+              {"-1", "-1", "-1"},
+              {"-1", "-1", "A"}
+            },
+            -1
+            //
+            ),
+        Arguments.of(
+            new int[] {3}, //
+            new String[][] {
+              {"-1", "-1", "A"}, //
+              {"-1", "-1", "-1"},
+              {"-1", "-1", "-1"}
+            },
+            -1
+            //
+            ),
+        //
+        Arguments.of(
+            new int[] {1}, //
+            new String[][] {
+              {"A"}, //
+            },
+            -1
+            //
+            ),
+        //
+        Arguments.of(
+            new int[] {1}, //
+            new String[][] {
+              {"-1"}, //
+            },
+            1
+            //
+            )
+        //
+        );
+  }
+}
---
+++ src/test/java/org/fp024/lv01/Exam340198Tests.java
@@ -0,0 +1,86 @@
+package org.fp024.lv01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/*
+ * [PCCE 기출문제] 10번 / 공원
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/340198
+ */
+@Slf4j
+class Exam340198Tests {
+  /** 문제 번호 */
+  private static final int EXAM_NO = 340198;
+
+  // ========== Target ==========
+  static class Solution {
+    public int solution(int[] mats, String[][] park) {
+      int answer = -1;
+      int maxSquare = 0;
+
+      // (1) 동적프로그래밍(DP)를 활용가능하도록 빈 공간을 1, 돗자리들이 차지한 영역을 0으로 바꾼다.
+      int[][] dp = new int[park.length][park[0].length];
+      for (int i = 0; i < park.length; i++) {
+        for (int j = 0; j < park[i].length; j++) {
+          dp[i][j] = park[i][j].equals("-1") ? 1 : 0;
+        }
+      }
+
+      // (2) 정사각형이 가능한 조건을 하나씩 채워나간다.
+      //     현재 위치에서 좌/상/좌상과 각각 더했을 때 최소값이 현재 위치에서 가능한 정사각형 길이
+      for (int i = 1; i < dp.length; i++) {
+        for (int j = 1; j < dp[i].length; j++) {
+          int up = dp[i - 1][j];
+          int left = dp[i][j - 1];
+          int upLeft = dp[i - 1][j - 1];
+
+          dp[i][j] += Math.min(Math.min(up, left), upLeft);
+        }
+      }
+
+      // (3) 최대 정사각형 한변 얻기
+      for (int i = 0; i < dp.length; i++) {
+        for (int j = 0; j < dp[i].length; j++) {
+          maxSquare = Math.max(maxSquare, dp[i][j]);
+        }
+      }
+
+      Arrays.sort(mats);
+      for (int i = mats.length - 1; i >= 0; i--) {
+        if (mats[i] <= maxSquare) {
+          answer = mats[i];
+          break;
+        }
+      }
+
+      return answer;
+    }
+  }
+
+  // ========== Test ==========
+  @ParameterizedTest
+  @MethodSource({
+    "org.fp024.lv01.Exam" + EXAM_NO + "TestData#defaultDataProvider", //
+    "org.fp024.lv01.Exam" + EXAM_NO + "TestData#extraDataProvider"
+  })
+  void testSolution(int[] mats, String[][] park, int expect) {
+    assertThat(new Solution().solution(mats, park)).isEqualTo(expect);
+  }
+
+  // cspell:enable
+  //
+  // === 문제 읽고 첫 느낌 ===
+  //   처음에 어떻게 해결해야할지 막막해서 코딩 테스트 책을 보았는데,
+  //   "가장 큰 정사각형 찾기" 풀이 해설을 보니, 뭔가 이해가 된다.
+  //   - https://school.programmers.co.kr/learn/courses/30/lessons/12905
+  //   지금 문제를 위 문제와 비슷하게 풀면 쉽게 해결 될 것 같다.
+  //   🤔 기본 테스트는 통과하는데, 제출시 다량 실패한다. 왜그럴까? 😂😂😂
+  //
+  // === 다른 사람 풀이 확인 이후 의견 ===
+  // ...
+  //
+}
