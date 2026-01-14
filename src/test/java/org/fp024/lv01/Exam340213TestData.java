64670791bb68e84a2abe2693d2b8705a834c10bf
fp024 <aopenasus@naver.com>
2026-01-15 02:25:18 +0900

    테스트 데이터 클래스에 unused 경고 억제 추가
    VSCode는 -Xlint:unused가 활성화 되어있지 않아 상관 없지만, IntelliJ에서는 저 옵션이 기본으로 켜져있어서, 경고가 출력된다.
    
    해당 경고 억제 코드 추가.

--- src/test/java/org/fp024/lv01/Exam340213TestData.java
+++ src/test/java/org/fp024/lv01/Exam340213TestData.java
@@ -3,6 +3,7 @@ package org.fp024.lv01;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
+@SuppressWarnings("unused") // JUnit MethodSource에서 리플렉션으로 접근하므로 사용되지 않는 것으로 오인될 수 있어 경고 억제
 class Exam340213TestData {
   // cspell:disable
   static Stream<Arguments> defaultDataProvider() {
--- src/test/java/org/fp024/lv02/Exam42584TestData.java
+++ src/test/java/org/fp024/lv02/Exam42584TestData.java
@@ -4,6 +4,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
+@SuppressWarnings("unused") // JUnit MethodSource에서 리플렉션으로 접근하므로 사용되지 않는 것으로 오인될 수 있어 경고 억제
 class Exam42584TestData {
   // cspell:disable
   static Stream<Arguments> defaultDataProvider() {
